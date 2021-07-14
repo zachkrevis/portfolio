29efa47a4d0dc66935ec2e8549c38917b81040b9
Zach Krevis <72327950+zachkrevis@users.noreply.github.com>
2021-07-14 09:32:14 -0400

    Working Menu Button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
+import React, {useState} from 'react';
 import Sidebar form '../components/Sidebar/';
 
 const Home = {} => {
+ const [isOpen, setIsOpen] = useState{false};
+ 
+ const toggle = {} => {
+  setIsOpen(!isOpen);
+  
+ 
  return {
    <>
-    <Sidebar />
-    <Navbar />
+    <Sidebar isOpen={isOpen} toggle={toggle}/>
+    <Navbar toggle={toggle}/>
    </>
  }
   
 }
+
+export default Home;
