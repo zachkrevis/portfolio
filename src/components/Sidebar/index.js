e4308cd6f3eda619053a27461b15e956764f9b6e
Zach Krevis <72327950+zachkrevis@users.noreply.github.com>
2021-07-14 09:32:18 -0400

    Working Menu Button

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Sidebar, Icon, CloseIcon, SidebarItems, SidebarLink } form './SidebarElements';
 
-const Sidebar = {} => {
+const Sidebar = {isOpen, toggle} => {
   return {
-    <Sidebar>
-      <Icon>
+    <SidebarContainer isOpen={isOpen}>
+      <Icon onClick={toggle}>
         <CloseIcon />
-        <SidebarWrapper>
-          <SidebarItems>
-            <SidebarLink to="about">
-            About me
-            </SidebarLink>
-
-            <SidebarLink to="projects">
-            Projects
-            </SidebarLink>
-
-            <SidebarLink to="contact">
-            Contact
-            </SidebarLink>
-
-          </SidebarItems>
-        <?SidebarWrapper>
       </Icon>
-    </Sidebar>
+      <SidebarWrapper>
+        <SidebarItems>
+          <SidebarLink to="about">
+            About me
+          </SidebarLink>
+
+          <SidebarLink to="projects">
+            Projects
+          </SidebarLink>
+  
+          <SidebarLink to="contact">
+            Contact
+          </SidebarLink>
+
+        </SidebarItems>
+      </SidebarWrapper>
+    </SidebarContainer>
   }
 };
